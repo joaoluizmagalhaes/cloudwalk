60489660aca970b1a82b743cf99e78f20a3e0e32
Joao Luiz de Figueiredo Magalhães <dukejlf@gmail.com>
2024-02-20 11:53:46 -0300

    creatd the header.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,17 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        customGray: '#E6E6E6'
+      },
+      fontFamily: {
+        sans: ['Helvetica Neue', 'Arial', 'sans-serif'],
+      },
+      fontSize: {
+        '1.5xl': '20px',
+        '3.5xl': '32px',
+        '5.5xl': '54px'
+      }
     },
   },
   plugins: [],
