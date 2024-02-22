fd85339c33258864ec82a3e09e16ac9a2aeddeda
Joao Luiz de Figueiredo Magalhães <dukejlf@gmail.com>
2024-02-22 11:25:07 -0300

    Refatcoer fetch planets and fetch people. Add load more button.

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -2,10 +2,13 @@ export async function GET(request) {
   
   const { searchParams } = new URL(request.url);
   const endpoint = searchParams.get('endpoint');
-  
-  const baseUrl = 'https://swapi.dev/api/';
-  const url = `${baseUrl}${endpoint}`;
+  const pageParam = searchParams.get('page');
 
+  const page = pageParam ? `?page=${pageParam}` : '';
+
+  const baseUrl = 'https://swapi.dev/api/';
+  const url = `${baseUrl}${endpoint}/${page}`;
+console.log(url)
   const res = await fetch(url, {
     headers: {
       'Content-Type': 'application/json',
@@ -14,7 +17,6 @@ export async function GET(request) {
 
   const data = await res.json();
   
-
   return new Response(JSON.stringify(data), {
     headers: { 'Content-Type': 'application/json' },
   });
--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,24 @@
 'use client'
 
-import { useEffect, useState } from 'react'
-
+import React, { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import Card from './components/Card'
 import Loading from './components/Loading'
 
-
 export default function Home() {
-
-  const [cardData, setCardData] = useState([]) 
+  const [cardData, setCardData] = useState([])
   const [planetOptions, setPlanetOptions] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [allPeople, setAllPeople] = useState([])
+  const [displayCount, setDisplayCount] = useState(8)
+  const [selectedFilter, setSelectedFilter] = useState('All')
+  const [currentPage, setCurrentPage] = useState(1)
+  const [planetsLoaded, setPlanetsLoaded] = useState(false)
+  const [totalPeopleCount, setTotalPeopleCount] = useState(0)
 
   useEffect(() => {
     setIsLoading(true)
-    setPlanetOptions([])
-    setCardData([])
-    setPlanetOptions(prevData => [...prevData, {label: 'All', value: 'All'}])
+    setPlanetOptions(prevData => [...prevData, { label: 'All', value: 'All' }])
 
     async function fetchPlanets(pageURL) {
       try {
@@ -34,6 +35,9 @@ export default function Home() {
         setPlanetOptions(prevData => [...prevData, ...enrichedData])
         if (data.next) {
           await fetchPlanets(data.next)
+        } else {
+          setPlanetsLoaded(true)
+          setIsLoading(false)
         }
       } catch (error) {
         console.error('Erro ao buscar dados:', error)
@@ -45,70 +49,93 @@ export default function Home() {
   }, [])
 
   useEffect(() => {
-    if (planetOptions.length > 0) {
-      async function fetchPeople() {
-        try {
-          const response = await fetch(`${window.location.origin}/api?endpoint=people`)
-          if (!response.ok) {
-            throw new Error('Falha na rede')
-          }
-          const data = await response.json()
-          const planetMap = planetOptions.reduce((map, planet) => {
-            map[planet.url] = planet.value
-            return map
-          }, {})
-          
-          const enrichedData = data.results.slice(0, 8).map(element => {
-            const planetName = planetMap[element.homeworld]
-            const random = Math.floor(Math.random() * 100)
-            return {
-              ...element,
-              homeworld: planetName,
-              imageURL: `https://picsum.photos/435/230?random=${random}`
-            }
-          })
-          setCardData(enrichedData)
-        } catch (error) {
-          console.error('Erro ao buscar dados:', error)
-        } finally {
-          setIsLoading(false)
-        }
-      }
+    if (planetsLoaded) {
       fetchPeople()
     }
-  }, [planetOptions])
+  }, [planetOptions, planetsLoaded, currentPage])
 
-  
-  const [selectedFilter, setSelectedFilter] = useState('All')
-  
+  async function fetchPeople() {
+    if (allPeople.length < totalPeopleCount || currentPage === 1) {
+      setIsLoading(true)
+      try {
+        const response = await fetch(`${window.location.origin}/api/?endpoint=people&page=${currentPage}`)
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        const data = await response.json()
+        if (currentPage === 1) {
+          setTotalPeopleCount(data.count)
+        }
+
+        const planetMap = planetOptions.reduce((acc, planet) => {
+          acc[planet.url] = planet.label
+          return acc
+        }, {})
+
+      
+        const newPeople = data.results.map(person => {
+
+          const random = Math.floor(Math.random() * 100)
+
+          return {
+            ...person,
+            homeworld: planetMap[person.homeworld] || 'Unknown Planet',
+            imageURL: `https://picsum.photos/435/230?random=${random}`
+          }
+          
+        })
+
+        setAllPeople(prev => [...prev, ...newPeople])
+      } catch (error) {
+        console.error("Failed to fetch people:", error)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+  }
+
+  const handleLoadMore = () => {
+    const newDisplayCount = displayCount + 8
+    if (newDisplayCount <= totalPeopleCount) {
+      setDisplayCount(newDisplayCount)
+      if (newDisplayCount > allPeople.length) {
+        setCurrentPage(prev => prev + 1)
+      }
+    }
+  }
+
   const handleFilterChange = (value) => {
     setSelectedFilter(value)
   }
 
   return (
     <main className="flex min-h-screen flex-col w-full py-20 relative">
-      {isLoading && <Loading /> }
-     <header className="flex flex-col w-full font-sans font-light mb-7 px-[25px] md:px-[50px]">
-      <h1 className="text-3.5xl md:text-5.5xl leading-10 md:leading-[64px] mb-2.5 md:mb-6">Star Wars Characters</h1>
-      <p className="text-base md:text-1.5xl leading-6 md:leading-[32px] tracking-[0.92px] max-w-[920px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-     </header>
-     <div className="h-14 md:h-[90px] flex justify-between items-center border-t md:border-y border-borderGray w-full px-[25px] md:px-[50px]">
-      <Filter options={planetOptions} onChange={handleFilterChange} />
-      <button className='hidden md:flex items-center uppercase border border-filterGray text-filterGray tracking-[0.8px] font-arial text-sm h-9 px-10'>Clear all</button>
-     </div>
-     <section className="p-[25px] md:p-[50px]">
-      <h1 className='capitalize font-sans text-darkGray font-light text-3.5xl md:text-3.75xl'>{selectedFilter} Characters</h1>
-      <div className='grid grid-cols-1 md:grid-cols-4 w-full gap-y-11 md:gap-8 md:gap-y-28 my-8 mb-11 md:mb-28'>
-        {cardData.map( data => {
-          return (
-            <Card data={data} key={data.name}/>
-          )
-        })}
+      {isLoading && <Loading />}
+      <header className="flex flex-col w-full font-sans font-light mb-7 px-[25px] md:px-[50px]">
+        <h1 className="text-3.5xl md:text-5.5xl leading-10 md:leading-[64px] mb-2.5 md:mb-6">Star Wars Characters</h1>
+        <p className="text-base md:text-1.5xl leading-6 md:leading-[32px] tracking-[0.92px] max-w-[920px]">
+          Explore the universe of Star Wars characters.
+        </p>
+      </header>
+      <div className="h-14 md:h-[90px] flex justify-between items-center border-t md:border-y border-borderGray w-full px-[25px] md:px-[50px]">
+        <Filter options={planetOptions} onChange={handleFilterChange} />
+        <button className='hidden md:flex items-center uppercase border border-filterGray text-filterGray tracking-[0.8px] font-arial text-sm h-9 px-10'>Clear all</button>
       </div>
-      <div className='flex justify-center'>
-        <button className='w-48 md:w-[486px] font-arial border border-textBlue text-textBlue text-sm uppercase h-11'>Load More</button>
-      </div>
-     </section>
+      <section className="p-[25px] md:p-[50px]">
+        <h1 className='capitalize font-sans text-darkGray font-light text-3.5xl md:text-3.75xl'>{selectedFilter} Characters</h1>
+        <div className='grid grid-cols-1 md:grid-cols-4 w-full gap-y-11 md:gap-8 md:gap-y-28 my-8 mb-11 md:mb-28'>
+          {allPeople.slice(0, displayCount).map((person, index) => (
+            <Card key={index} data={person} />
+          ))}
+        </div>
+        {displayCount < totalPeopleCount && (
+          <div className='flex justify-center'>
+            <button onClick={handleLoadMore} className='w-48 md:w-[486px] font-arial border border-textBlue text-textBlue text-sm uppercase h-11'>
+              Load More
+            </button>
+          </div>
+        )}
+      </section>
     </main>
   )
 }
